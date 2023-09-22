a6f87c0a9ccb8149cd93720779af81e2be34335c
Danilo Moura <danilo.moura@luizalabs.com>
2023-09-22 13:25:47 -0300

    Ajustes deploy

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import SearchButton from './SearchButton'
+
 
 const Header = () => {
   return (
@@ -36,8 +36,7 @@ const Header = () => {
             >
               {link.title}
             </Link>
-          ))}
-        <SearchButton />
+          ))}        
         <ThemeSwitch />
         <MobileNav />
       </div>
